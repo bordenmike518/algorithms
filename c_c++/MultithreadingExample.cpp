8b38da92858345eb619ec8d41debd30c658afbc9
Michael Borden <bordenmike518@gmail.com>
2019-02-20 16:34:11 -0500

    fixed output issue do to multithreading

--- c_c++/MultithreadingExample.cpp
+++ c_c++/MultithreadingExample.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
-#include <cstdlib>
 #include <pthread.h>
 
 using namespace std;
 
-#define NUM_THREADS 2
+#define NUM_THREADS 6
 
-void *multithreadingExample(void *threadId) {
-    long tid = (long) threadId;
-    for(int i = 0; i < 5; i++)
-        cout << "Hello from thread " << tid << ":  " << i << endl;
-    pthread_exit(NULL);
-}
+void *multithreadingExample(void *threadId);
 
 int main(int argc, char **argv) {
     pthread_t threads[NUM_THREADS];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    int pt;
 
     for(long i = 0; i < NUM_THREADS; i++) {
-        int pt = pthread_create(&threads[i], NULL, multithreadingExample, (void *)i);
+        pt = pthread_create(&threads[i], &attr, multithreadingExample, (void *)i);
         if (pt) {
             cout << "Error: unable to create thread " << pt << endl;
             exit(-1); 
         }
     }
-    pthread_exit(NULL);
+
+    for (int i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+
     cout << "Closing" << endl;
+}
+
+void *multithreadingExample(void *threadId) {
+    long tid = (long) threadId;
+    string message;
+    for(int i = 0; i < 10; i++) {
+        message = "Hello from thread " + to_string(tid) + ":  " + to_string(i) + "\n";
+        cout << message;
+        /* Multithreading print out issue example */
+        //cout << "Hello from thread " << tid << ":  " << i << endl;
+    }
+    pthread_exit(NULL);
 }
